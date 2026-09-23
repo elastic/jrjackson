cdaf9020a83ffe668ab77e3668bd3fef523590b1
andsel <selva.andre@gmail.com>
2026-09-23 14:49:27 +0200

    Update Jackson to 2.21.7

--- lib/jrjackson/build_info.rb
+++ lib/jrjackson/build_info.rb
@@ -1,11 +1,11 @@
 module JrJackson
   module BuildInfo
     def self.version
-      '0.5.3'
+      '0.5.4'
     end
 
     def self.release_date
-      '2026-09-16'
+      '2026-09-23'
     end
 
     def self.files
@@ -13,7 +13,7 @@ module JrJackson
     end
 
     def self.jackson_version
-      '2.21.6'
+      '2.21.7'
     end
 
     def self.jackson_annotations_version
@@ -21,7 +21,7 @@ module JrJackson
     end
 
     def self.jackson_databind_version
-      '2.21.6'
+      '2.21.7'
     end
 
     def self.jar_version
